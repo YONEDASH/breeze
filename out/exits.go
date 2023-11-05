fee2b028bf77b7771db7fb2e3f9110e7a877509c
YONEDASH <til211@gmx.de>
2023-11-05 17:08:25 +0100

    Exit codes based on sysexits.h

---
+++ out/exits.go
@@ -0,0 +1,22 @@
+package out
+
+// Based on https://man.openbsd.org/sysexits
+
+const (
+	ExOk          = 0
+	ExUsage       = 64
+	ExDataErr     = 65
+	ExNoInput     = 66
+	ExNoUser      = 67
+	ExNoHost      = 68
+	ExUnavailable = 69
+	ExSoftware    = 70
+	ExOsErr       = 71
+	ExOsFile      = 72
+	ExCantCreat   = 73
+	ExIoErr       = 74
+	ExTempFail    = 75
+	ExProtocol    = 76
+	ExNoPerm      = 77
+	ExConfig      = 78
+)
